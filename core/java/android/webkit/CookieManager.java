5d4ece260c058a4fc0f6a2e848abbf0234c42bc5
fagyi <fagyipda@gmail.com>
2014-09-08 14:22:16 +0200

    [3/4] reduce cookie tracking: count cookies
    add an efficient cookie counting function

--- core/java/android/webkit/CookieManager.java
+++ core/java/android/webkit/CookieManager.java
@@ -135,6 +135,14 @@ public class CookieManager {
     }
 
     /**
+     * Counts number of cookies
+     * @hide
+     */
+    public int countCookies() {
+        throw new MustOverrideException();
+    }
+
+    /**
      * Removes all cookies.
      */
     public void removeAllCookie() {
